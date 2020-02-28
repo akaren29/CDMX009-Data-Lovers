2baee1e2402a87062d21b8a8a0f68b37195abc09
karenFlores <reika_299@hotmail.com>
2020-02-28 12:55:19 -0600

    terminada pagina de inicio

--- src/main.js
+++ src/main.js
@@ -2,3 +2,13 @@ import { example } from './data.js';
 
 console.log(example);
 
+let botonacceder = document.getElementById('acceder');
+botonacceder.addEventListener('click', obtenerNombre);
+
+
+function obtenerNombre  (name) {
+   let nameresult = document.getElementById('namecontent').value;
+    if (nameresult.length > 0){
+       document.getElementById('paginaContenedorNombre').innerText=('Hola ' + nameresult + ' Bienvenid@');
+    }
+}
