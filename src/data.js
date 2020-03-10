c2ea3e4a38c75e5ebf10684d7ea6468faf834d7e
karenFlores <reika_299@hotmail.com>
2020-03-09 18:41:45 -0600

    diseño y acomodo de cards

--- src/data.js
+++ src/data.js
@@ -1,13 +1,33 @@
 // import data from './data/injuries/injuries.js';
 // import data from './data/lol/lol.js';
 // import data from './data/patient/patient.js';
-import data from './data/pokemon/pokemon.js';
+import POKEMON from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 // import data from './data/steam/steam.js';
 // import data from './data/steam/worldbank.js';
 
 // esta es una función de ejemplo
 
-export const example = () => {
-  return 'example';
-};
+
+
+const data = window.POKEMON.pokemon;
+window.data = data;
+
+const order = (data, selectedOrder) => {
+    let filterOrder = "";
+    if (selectedOrder === "1-151") {
+        filterOrder = data.sort((a, b) => {
+            return a.id - b.id
+        })} else if (selectedOrder === "151-1") {
+            filterOrder = data.sort((a, b) => {
+            return b.id - a.id
+        })} else if (selectedOrder === "A-Z") {
+            filterOrder = data.sort((a, b) => {
+            return a.name.localeCompare(b.name);
+        })} else if (selectedOrder === "Z-A") {
+            filterOrder = data.sort((a, b) => {
+            return b.name.localeCompare(a.name);
+        })}
+        return filterOrder;
+}
+window.order = order;
--- src/main.js
+++ src/main.js
@@ -1,17 +1,37 @@
-//
-import pokemon from './data/pokemon/pokemon.js';
+import POKEMON from './data/pokemon/pokemon.js';
+ console.log(POKEMON.pokemon);
 
-// funcion traer nombre
-
-let botonacceder = document.getElementById('acceder');
-botonacceder.addEventListener('click', obtenerNombre);
-
-
-function obtenerNombre  (name) {
+ // funcion traer nombre
+let obtenerNombre = name => {
    let nameresult = document.getElementById('namecontent').value;
     if (nameresult.length > 0){
        document.getElementById('paginaContenedorNombre').innerText=('Hola ' + nameresult + ' Bienvenid@');
     }
 }
+let botonacceder = document.getElementById('acceder');
+botonacceder.addEventListener('click', obtenerNombre);
 
-console.log(pokemon);
+
+//Funcion traer pokemones a pagina contenedor
+ const data = POKEMON.pokemon;
+ const template = (list) => {
+     let templateListPokemon= '';
+   list.forEach((data) => {
+       const card = `
+       <div class = 'card-link'>
+         <article class='container-cards id='container-cards'>
+         <div class="cuadro-dentro">
+                   <div class='contimg'><img class='img' src='${data.img}' /></div>
+                   <div class='article-details'>
+                   <h3 class='num' > ${data.num} </h3>
+                   <h4 class='nombre'>${data.name}</h4>
+                   <h6 class='tipo'>Tipo: ${data.type}</h6>
+                 </div>
+         </article>
+        </div>
+       `;
+    templateListPokemon += card;
+   });
+   document.getElementById('container-cards-pokemon').innerHTML = templateListPokemon
+ };
+ template(data);
