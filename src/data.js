bd501319fd8f55d284234439753606b55e560b1f
karenFlores <reika_299@hotmail.com>
2020-03-10 08:40:11 -0600

    cambios en html

--- src/data.js
+++ src/data.js
@@ -7,28 +7,3 @@ import POKEMON from './data/pokemon/pokemon.js';
 // import data from './data/steam/worldbank.js';
 
 // esta es una función de ejemplo
-
-
-
-const pokeData = window.POKEMON.pokemon;
-window.pokeData = pokeData;
-
-//Ordenar
-const order = (pokeData, selectedOrder) => {
-    let filterOrder = "";
-    if (selectedOrder === "1-151") {
-        filterOrder = pokeData.sort((a, b) => {
-            return a.id - b.id
-        })} else if (selectedOrder === "151-1") {
-            filterOrder = pokeData.sort((a, b) => {
-            return b.id - a.id
-        })} else if (selectedOrder === "A-Z") {
-            filterOrder = pokeData.sort((a, b) => {
-            return a.name.localeCompare(b.name);
-        })} else if (selectedOrder === "Z-A") {
-            filterOrder = pokeData.sort((a, b) => {
-            return b.name.localeCompare(a.name);
-        })}
-        return filterOrder;
-}
-window.order = order;
--- src/main.js
+++ src/main.js
@@ -37,31 +37,4 @@ botonacceder.addEventListener('click', obtenerNombre);
  template(data);
 
 
- //Ordenar
- const selectOrder = document.getElementById("order");
- selectOrder.addEventListener("change", showOrder);
- function showOrder() {
-     let selectedOrder = selectOrder.options[selectOrder.selectedIndex].value;
-     let filterResult = window.order(window.data, selectedOrder);
-     document.getElementById('container-cards-pokemon').innerHTML = "";
-     for (let i = 0; i < filterResult.length; i++) {
-         let pokeCards = document.createElement('div');
-         pokeCards.className = 'card-link';
-         let pokeName = document.createElement('h4');
-         pokeName.textContent = filterResult[i].name;
-         let image = document.createElement('img}');
-         image.src = filterResult[i].img;
-         let pokeNumber = document.createElement('h3');
-         pokeNumber.textContent = filterResult[i].num;
-         let pokeType = document.createElement('h6');
-         pokeType.textContent = filterResult[i].type[0];
-         pokeCards.appendChild(image);
-         pokeCards.appendChild(pokeName);
-         pokeCards.appendChild(pokeNumber);
-         pokeCards.appendChild(pokeType);
-         //Mostrar tarjeta en HTLM -> section
-         document.getElementById('container-cards-pokemon').appendChild(pokeCards).innerHTML;
-     }
-  }
-
-search.addEventListener("keyup", filter);
+ 
