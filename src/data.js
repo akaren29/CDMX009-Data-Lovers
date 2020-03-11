a11e57fadb2680cc58c629a3c7ebd52bebbe13ad
karenFlores <reika_299@hotmail.com>
2020-03-11 09:49:18 -0600

    cambios en html y inicio de funcion filtrar

--- src/data.js
+++ src/data.js
@@ -1,9 +1,25 @@
 // import data from './data/injuries/injuries.js';
 // import data from './data/lol/lol.js';
 // import data from './data/patient/patient.js';
-import POKEMON from './data/pokemon/pokemon.js';
+import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 // import data from './data/steam/steam.js';
 // import data from './data/steam/worldbank.js';
 
 // esta es una función de ejemplo
+
+const filterData = (data, condition) => {
+  let arrType = [];
+  for (let i = 0; i < data.length; i++) {
+    for (let x = 0; x < data[i].type.length; x++) {
+      if (data[i].type[x] === condition) {
+        arrType.push(data[i]);
+      }
+    }
+  }
+  return arrType;
+};
+
+window.POKE = {
+  filterData,
+};
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import POKEMON from './data/pokemon/pokemon.js';
- console.log(POKEMON.pokemon);
+import data from './data/pokemon/pokemon.js';
+ console.log(data.pokemon);
 
  // funcion traer nombre
 let obtenerNombre = name => {
@@ -13,19 +13,19 @@ botonacceder.addEventListener('click', obtenerNombre);
 
 
 //Funcion traer pokemones a pagina contenedor
- const data = POKEMON.pokemon;
- const template = (list) => {
+ const pokemones = data.pokemon;
+ const template = () => {
      let templateListPokemon= '';
-   list.forEach((data) => {
+   pokemones.forEach((pokemon) => {
        const card = `
        <div class = 'card-link'>
          <article class='container-cards id='container-cards'>
          <div class="cuadro-dentro">
-                   <div class='contimg'><img class='img' src='${data.img}' /></div>
+                   <div class='contimg'><img class='img' src='${pokemon.img}' /></div>
                    <div class='article-details'>
-                   <h3 class='num' > ${data.num} </h3>
-                   <h4 class='nombre'>${data.name}</h4>
-                   <h6 class='tipo'>Tipo: ${data.type}</h6>
+                   <h3 class='num' > ${pokemon.num} </h3>
+                   <h4 class='nombre'>${pokemon.name}</h4>
+                   <h6 class='tipo'>Tipo: ${pokemon.type}</h6>
                  </div>
          </article>
         </div>
@@ -34,7 +34,41 @@ botonacceder.addEventListener('click', obtenerNombre);
    });
    document.getElementById('container-cards-pokemon').innerHTML = templateListPokemon
  };
- template(data);
+ template(pokemones);
 
 
- 
+//filtrado por tipo
+document.getElementById('typePokemones').addEventListener('change', (even) => {
+  const selectedIndex = even.currentTarget.selectedIndex;
+  if (selectedIndex === 1) {
+    template(POKE.filterData(pokemones, 'Water'));
+  } else if (selectedIndex === 2) {
+    template(POKE.filterData(pokemones, 'Bug'));
+  } else if (selectedIndex === 3) {
+    template(POKE.filterData(pokemones, 'Dragon'));
+  } else if (selectedIndex === 4) {
+    template(POKE.filterData(pokemones, 'Electric'));
+  } else if (selectedIndex === 5) {
+    template(POKE.filterData(pokemones, 'Ghost'));
+  } else if (selectedIndex === 6) {
+    template(POKE.filterData(pokemones, 'Fire'));
+  } else if (selectedIndex === 7) {
+    template(POKE.filterData(pokemones, 'Ice'));
+  } else if (selectedIndex === 8) {
+    template(POKE.filterData(pokemones, 'Fighting'));
+  } else if (selectedIndex === 9) {
+    template(POKE.filterData(pokemones, 'Normal'));
+  } else if (selectedIndex === 10) {
+    template(POKE.filterData(pokemones, 'Grass'));
+  } else if (selectedIndex === 11) {
+    template(POKE.filterData(pokemones, 'Psychic'));
+  } else if (selectedIndex === 12) {
+    template(POKE.filterData(pokemones, 'Rock'));
+  } else if (selectedIndex === 13) {
+    template(POKE.filterData(pokemones, 'Ground'));
+  } else if (selectedIndex === 14) {
+    template(POKE.filterData(pokemones, 'Poison'));
+  } else if (selectedIndex === 15) {
+    template(POKE.filterData(pokemones , 'Flying'));
+  }
+})
