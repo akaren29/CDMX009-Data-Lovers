9abe98507e59ae2567138a9f6e3fdb44a152c844
akaren29 <akjassoj@outlook.com>
2020-03-11 09:50:57 -0600

    Se implemento función ordenar

--- src/data.js
+++ src/data.js
@@ -1,15 +1,39 @@
-// import data from './data/injuries/injuries.js';
-// import data from './data/lol/lol.js';
-// import data from './data/patient/patient.js';
- import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
-// import data from './data/steam/steam.js';
-// import data from './data/steam/worldbank.js';
 
-// esta es una función de ejemplo
+ const sortData = (data, sortBy, sortOrder) => {
+   let arrOrder = [];
+   /*Ordenar Alfabeticamente*/
+    if (sortBy === 'name'){
+       if (sortOrder === 'A-Z'){
+         arrOrder = data.sort(function(abc,bcd){
+            if (abc.name > bcd.name) return 1;
+            if (abc.name === bcd.name) return 0;
+            return -1; 
+         });
+       }else{
+         arrOrder = data.sort(function (abc, bcd){
+           if (bcd.name > abc.name) return 1;
+           if (bcd.name === abc.name) return 0;
+           return -1;
+         });
+       }
+     /*Ordenar por número*/
+     } else if (sortBy === 'number'){
+       if (sortOrder === 'asc'){
+         arrOrder = data.sort(function (abc, bcd){
+           if(abc.num > bcd.num) return 1;
+           if(abc.num === bcd.num) return 0;
+           return -1;
+         });
+       }else {
+         arrOrder = data.sort(function(abc, bcd){
+           if (bcd.num > abc.num) return 1;
+           if (bcd.num === abc.num) return 0;
+           return -1;
+         });
+       }
+     }
+    return arrOrder;
+    };
 
-//Traer nombre
-
-export const example = () => {
-  return 'example';
-};
+ 
+export default sortData
--- src/main.js
+++ src/main.js
@@ -1,36 +1,55 @@
- import POKEMON from './data/pokemon/pokemon.js';
- console.log(POKEMON.pokemon);
+import data from './data/pokemon/pokemon.js';
+import sorData from './data.js';
+//console.log(data.pokemon);
 
- // funcion traer nombre
+// funcion traer nombre
 let obtenerNombre = name => {
-   let nameresult = document.getElementById('namecontent').value;
-    if (nameresult.length > 0){
-       document.getElementById('paginaContenedorNombre').innerText=('Hola ' + nameresult + ' Bienvenid@');
-    }
+  let nameresult = document.getElementById('namecontent').value;
+   if (nameresult.length > 0){
+      document.getElementById('paginaContenedorNombre').innerText=('Hola ' + nameresult + ' Bienvenid@');
+   }
 }
 let botonacceder = document.getElementById('acceder');
-botonacceder.addEventListener('click', obtenerNombre); 
+botonacceder.addEventListener('click', obtenerNombre);
 
 
 //Funcion traer pokemones a pagina contenedor
- const data = POKEMON.pokemon;
- const template = (list) => {
-     let templateListPokemon= '';
-   list.forEach((data) => {
-       const card = `
-       <div class = 'card-link'>
-         <article class='container-cards id='container-cards'>
-          <img class='post-image' src='${data.img}' />
-           <h3 class='post-id' > ${data.num} </h3>
-           <div class='article-details'>
-            <h4 class='post-title'>${data.name}</h4>
-            <h6 class='post-category'>${data.type}</h6>
-           </div>
-         </article>
-        </div>
-       `;
-    templateListPokemon += card;
-   });
-   document.getElementById('container-cards-pokemon').innerHTML = templateListPokemon
- };
- template(data);
+const pokemones = data.pokemon;
+const template = () => {
+    let templateListPokemon= '';
+  pokemones.forEach((pokemon) => {
+      const card = `
+      <div class = 'card-link'>
+        <article class='container-cards id='container-cards'>
+        <div class="cuadro-dentro">
+                  <div class='contimg'><img class='img' src='${pokemon.img}' /></div>
+                  <div class='article-details'>
+                  <h3 class='num' > ${pokemon.num} </h3>
+                  <h4 class='nombre'>${pokemon.name}</h4>
+                  <h6 class='tipo'>Tipo: ${pokemon.type}</h6>
+                </div>
+        </article>
+       </div>
+      `;
+   templateListPokemon += card;
+  });
+  document.getElementById('container-cards-pokemon').innerHTML = templateListPokemon
+};
+template(pokemones);
+
+
+ //Función para ordenar a los pokemones
+  document.getElementById('orderPokemon').addEventListener( 'click', (event) =>{
+   const selectedIndex = event.currentTarget.selectedIndex;
+   if (selectedIndex === 1){
+     template(sorData(pokemones, 'name', 'A-Z'));
+   }else if (selectedIndex === 2){
+     template(sorData(pokemones, 'name', 'Z-A'));
+   }else if (selectedIndex === 3){
+     template(sorData(pokemones, 'number', 'asc'));
+   }else if(selectedIndex === 4){
+     template(sorData(pokemones, 'number', 'des'));
+   }
+ }); 
+
+ 
