7c8a51de3a352ba78d4c1123a10242b076d191b8
karenFlores <reika_299@hotmail.com>
2020-03-13 13:13:34 -0600

    cambios finales

--- src/data.js
+++ src/data.js
@@ -1,51 +1,42 @@
-// import data from './data/injuries/injuries.js';
-// import data from './data/lol/lol.js';
-// import data from './data/patient/patient.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
-// import data from './data/steam/steam.js';
-// import data from './data/steam/worldbank.js';
-
-// esta es una función de ejemplo
 const sortData = (data, sortBy, sortOrder) => {
-  let arrOrder = [];
-  /*Ordenar Alfabeticamente*/
-   if (sortBy === 'name'){
-      if (sortOrder === 'A-Z'){
-        arrOrder = data.sort(function(abc,bcd){
-           if (abc.name > bcd.name) return 1;
-           if (abc.name === bcd.name) return 0;
+   let arrOrder = [];
+   /*Ordenar Alfabeticamente*/
+    if (sortBy === 'name'){
+       if (sortOrder === 'A-Z'){
+         arrOrder = data.sort(function(abc,bcd){
+            if (abc.name > bcd.name) return 1;
+            if (abc.name === bcd.name) return 0;
+            return -1;
+         });
+       }else{
+         arrOrder = data.sort(function (abc, bcd){
+           if (bcd.name > abc.name) return 1;
+           if (bcd.name === abc.name) return 0;
            return -1;
-        });
-      }else{
-        arrOrder = data.sort(function (abc, bcd){
-          if (bcd.name > abc.name) return 1;
-          if (bcd.name === abc.name) return 0;
-          return -1;
-        });
-      }
-      
-    /*Ordenar por número*/
-    } else if (sortBy === 'number'){
-      if (sortOrder === 'asc'){
-        arrOrder = data.sort(function (abc, bcd){
-          if(abc.num > bcd.num) return 1;
-          if(abc.num === bcd.num) return 0;
-          return -1;
-        });
-      }else {
-        arrOrder = data.sort(function(abc, bcd){
-          if (bcd.num > abc.num) return 1;
-          if (bcd.num === abc.num) return 0;
-          return -1;
-        });
-      }
-    }
-   return arrOrder;
-   };
+         });
+       }
+     /*Ordenar por número*/
+     } else if (sortBy === 'number'){
+       if (sortOrder === 'asc'){
+         arrOrder = data.sort(function (abc, bcd){
+           if(abc.num > bcd.num) return 1;
+           if(abc.num === bcd.num) return 0;
+           return -1;
+         });
+       }else {
+         arrOrder = data.sort(function(abc, bcd){
+           if (bcd.num > abc.num) return 1;
+           if (bcd.num === abc.num) return 0;
+           return -1;
+         });
+       }
+     }
+    return arrOrder;
+    };
 
 
-export default sortData
+
+
 
 //funcion filtrar
 
@@ -60,3 +51,5 @@ const filterData = (data, condition) => {
   }
   return arrType;
 };
+
+export {sortData, filterData}
--- src/main.js
+++ src/main.js
@@ -1,89 +1,111 @@
 import data from './data/pokemon/pokemon.js';
- console.log(data.pokemon);
+import {sortData,filterData} from './data.js';
+console.log(data.pokemon);
 
- // funcion traer nombre
+// funcion traer nombre
 let obtenerNombre = name => {
-   let nameresult = document.getElementById('namecontent').value;
-    if (nameresult.length > 0){
-       document.getElementById('paginaContenedorNombre').innerText=('Hola ' + nameresult + ' Bienvenid@');
-    }
+  let nameresult = document.getElementById('namecontent').value;
+   if (nameresult.length > 0){
+      document.getElementById('paginaContenedorNombre').innerText=('Hola ' + nameresult + ' Bienvenid@');
+   }
 }
 let botonacceder = document.getElementById('acceder');
 botonacceder.addEventListener('click', obtenerNombre);
 
 
-//Función para ordenar a los pokemones
+
+//Funcion traer pokemones a pagina contenedor
+const pokemones = data.pokemon;
+const template = (list) => {
+    let templateListPokemon= '';
+  list.forEach((pokemones) => {
+      const card = `
+      <div class = 'card-link'>
+        <article class='container-cards id='container-cards'>
+        <div class="cuadro-dentro">
+                  <div class='contimg'><img class='img' src='${pokemones.img}' /></div>
+                  <div class='article-details'>
+                  <h3 class='num' > ${pokemones.num} </h3>
+                  <h4 class='nombre'>${pokemones.name}</h4>
+                  <h6 class='tipo'>Tipo: ${pokemones.type}</h6>
+                </div>
+        </article>
+       </div>
+      `;
+   templateListPokemon += card;
+  });
+  document.getElementById('container-cards-pokemon').innerHTML = templateListPokemon
+};
+template(pokemones);
+
+
+ //Función para ordenar a los pokemones
   document.getElementById('orderPokemon').addEventListener( 'click', (event) =>{
    const selectedIndex = event.currentTarget.selectedIndex;
    if (selectedIndex === 1){
-     template(sorData(pokemones, 'name', 'A-Z'));
+     template(sortData(pokemones, 'name', 'A-Z'));
    }else if (selectedIndex === 2){
-     template(sorData(pokemones, 'name', 'Z-A'));
+     template(sortData(pokemones, 'name', 'Z-A'));
    }else if (selectedIndex === 3){
-     template(sorData(pokemones, 'number', 'asc'));
+     template(sortData(pokemones, 'number', 'asc'));
    }else if(selectedIndex === 4){
-     template(sorData(pokemones, 'number', 'des'));
+     template(sortData(pokemones, 'number', 'des'));
    }
  });
 
 
-//Funcion traer pokemones a pagina contenedor
- const pokemones = data.pokemon;
- const template = () => {
-     let templateListPokemon= '';
-   pokemones.forEach((pokemon) => {
-       const card = `
-       <div class = 'card-link'>
-         <article class='container-cards id='container-cards'>
-         <div class="cuadro-dentro">
-                   <div class='contimg'><img class='img' src='${pokemon.img}' /></div>
-                   <div class='article-details'>
-                   <h3 class='num' > ${pokemon.num} </h3>
-                   <h4 class='nombre'>${pokemon.name}</h4>
-                   <h6 class='tipo'>Tipo: ${pokemon.type}</h6>
-                 </div>
-         </article>
-        </div>
-       `;
-    templateListPokemon += card;
-   });
-   document.getElementById('container-cards-pokemon').innerHTML = templateListPokemon
- };
- template(pokemones);
+ //filtrado por tipo
+ document.getElementById('typePokemones').addEventListener('change', (event) => {
+   const selectedIndex = event.currentTarget.selectedIndex;
+   if (selectedIndex === 1) {
+     template(filterData(pokemones, 'Water'));
+   } else if (selectedIndex === 2) {
+     template(filterData(pokemones, 'Bug'));
+   } else if (selectedIndex === 3) {
+     template(filterData(pokemones, 'Dragon'));
+     console.log((filterData(pokemones, 'Dragon')))
+   } else if (selectedIndex === 4) {
+     template(filterData(pokemones, 'Electric'));
+   } else if (selectedIndex === 5) {
+     template(filterData(pokemones, 'Ghost'));
+   } else if (selectedIndex === 6) {
+     template(filterData(pokemones, 'Fire'));
+   } else if (selectedIndex === 7) {
+     template(filterData(pokemones, 'Ice'));
+   } else if (selectedIndex === 8) {
+     template(filterData(pokemones, 'Fighting'));
+   } else if (selectedIndex === 9) {
+     template(filterData(pokemones, 'Normal'));
+   } else if (selectedIndex === 10) {
+     template(filterData(pokemones, 'Grass'));
+   } else if (selectedIndex === 11) {
+     template(filterData(pokemones, 'Psychic'));
+   } else if (selectedIndex === 12) {
+     template(filterData(pokemones, 'Rock'));
+   } else if (selectedIndex === 13) {
+     template(filterData(pokemones, 'Ground'));
+   } else if (selectedIndex === 14) {
+     template(filterData(pokemones, 'Poison'));
+   } else if (selectedIndex === 15) {
+     template(filterData(pokemones , 'Flying'));
+   }
+ });
 
 
-//filtrado por tipo
-document.getElementById('typePokemones').addEventListener('change', (even) => {
-  const selectedIndex = even.currentTarget.selectedIndex;
-  if (selectedIndex === 1) {
-    template(POKE.filterData(pokemones, 'Water'));
-  } else if (selectedIndex === 2) {
-    template(POKE.filterData(pokemones, 'Bug'));
-  } else if (selectedIndex === 3) {
-    template(POKE.filterData(pokemones, 'Dragon'));
-  } else if (selectedIndex === 4) {
-    template(POKE.filterData(pokemones, 'Electric'));
-  } else if (selectedIndex === 5) {
-    template(POKE.filterData(pokemones, 'Ghost'));
-  } else if (selectedIndex === 6) {
-    template(POKE.filterData(pokemones, 'Fire'));
-  } else if (selectedIndex === 7) {
-    template(POKE.filterData(pokemones, 'Ice'));
-  } else if (selectedIndex === 8) {
-    template(POKE.filterData(pokemones, 'Fighting'));
-  } else if (selectedIndex === 9) {
-    template(POKE.filterData(pokemones, 'Normal'));
-  } else if (selectedIndex === 10) {
-    template(POKE.filterData(pokemones, 'Grass'));
-  } else if (selectedIndex === 11) {
-    template(POKE.filterData(pokemones, 'Psychic'));
-  } else if (selectedIndex === 12) {
-    template(POKE.filterData(pokemones, 'Rock'));
-  } else if (selectedIndex === 13) {
-    template(POKE.filterData(pokemones, 'Ground'));
-  } else if (selectedIndex === 14) {
-    template(POKE.filterData(pokemones, 'Poison'));
-  } else if (selectedIndex === 15) {
-    template(POKE.filterData(pokemones , 'Flying'));
-  }
-})
+ // se muestra la segunda y desaparece la primera seccion
+   document.getElementById("acceder").addEventListener('click', () => {
+   document.getElementById("segunda-seccion").style.display = 'block';
+   document.getElementById("header-inicio").style.display = 'none';
+ });
+
+ // se muestra la segunda y desaparece la primera seccion
+   document.getElementById("verOrder").addEventListener('click', () => {
+   document.getElementById("segunda-seccion").style.display = 'block';
+   document.getElementById("header-inicio").style.display = 'none';
+ });
+
+ // se muestra la segunda y desaparece la primera seccion
+   document.getElementById("verFiltro").addEventListener('click', () => {
+   document.getElementById("segunda-seccion").style.display = 'block';
+   document.getElementById("header-inicio").style.display = 'none';
+ });
